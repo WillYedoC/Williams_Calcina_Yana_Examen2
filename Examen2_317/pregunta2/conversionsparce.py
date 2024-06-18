6c5b119433cc404c001242d7df45908db9ea1780
WillYedoC <willyedoc123@gmail.com>
2024-06-18 15:31:07 -0400

    Update conversionsparce.py

--- Examen2_317/pregunta2/conversionsparce.py
+++ Examen2_317/pregunta2/conversionsparce.py
@@ -1,37 +1,23 @@
-# Importar las bibliotecas necesarias
 import numpy as np
-import plotly.express as px
-from PIL import Image
+import scipy.sparse as sp
+import cv2
 from google.colab import drive
 
-# Montar Google Drive
 drive.mount('/content/drive')
 
-# Definir la ruta a la imagen
-image_path = '/content/drive/My Drive/imagenes/jirafa.jpg'
+image_path1 = '/content/drive/My Drive/imagenes/jirafa.jpg'
 image_path2 = '/content/drive/My Drive/imagenes/HombrePalo.jpg'
-# Abrir la imagen
-imagen = Image.open(image_path)
-imagen2 = Image.open(image_path2)
-# Convertir la imagen a escala de grises si no lo está
-imagen_gr = imagen.convert('L')
-imagen_gr2 = imagen2.convert('L')
-# Convertir la imagen a una matriz de valores de píxeles
-imagen_mat = np.array(list(imagen_gr.getdata(band=0)), float)
-imagen_mat2 = np.array(list(imagen_gr2.getdata(band=0)), float)
 
-print(imagen_mat,imagen_mat2)
+img1 = cv2.imread(image_path1)
+img2 = cv2.imread(image_path2)
 
-# Ajustar la forma de la matriz para que coincida con la resolución de la imagen
-imagen_mat.shape = (imagen_gr.size[1], imagen_gr.size[0])
-imagen_mat2.shape = (imagen_gr2.size[1], imagen_gr2.size[0])
+img1_gray = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+img2_gray = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
 
-print(imagen_mat,imagen_mat2)
+m_sparce1 = sp.coo_matrix(img1_gray)
+m_sparce2 = sp.coo_matrix(img2_gray)
 
-# Verificar la forma de la matriz
-print(imagen_mat.shape,imagen_mat2.shape)
-
-# Mostrar la imagen usando plotly.express
-#fig = px.imshow(imagen_mat, color_continuous_scale='gray')
-fig = px.imshow(imagen_mat2, color_continuous_scale='gray')
-fig.show()
+print("Primera imagen (jirafa):")
+print(m_sparce1)
+print("Segunda imagen (HombrePalo):")
+print(m_sparce2)
